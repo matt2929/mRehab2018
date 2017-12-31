a17ea1d9b1dfdcd595057225cf0081023f85087f
matt2929 <mcstaffo@buffalo.edu>
2017-12-31 16:15:53 -0500

    Goals---- save goals update them after a workout load them before you pick workouts Reps--- before you start a workout load last number of reps completed allow use to pick how many they want to do Post Workout Report---- *after workout there is a report screen * shows #reps duration and accuracy compares to last time *Red = worse Green = Better yellow=? maybe add yellow

---
+++ app/src/main/java/com/example/matt2929/strokeappdec2017/Activity/PostWorkoutGoals.java
@@ -0,0 +1,15 @@
+package com.example.matt2929.strokeappdec2017.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.matt2929.strokeappdec2017.R;
+
+public class PostWorkoutGoals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_workout_goals);
+    }
+}
