c87e980627c3a7e54dd19dfd493763c154df5f94
matt2929 <mcstaffo@buffalo.edu>
2018-01-29 15:51:56 -0500

    text to speech description of how you did

--- app/src/main/java/com/example/matt2929/strokeappdec2017/Activity/PostWorkoutReportActivity.java
+++ app/src/main/java/com/example/matt2929/strokeappdec2017/Activity/PostWorkoutReportActivity.java
@@ -4,26 +4,37 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.matt2929.strokeappdec2017.ListenersAndTriggers.SpeechInitListener;
 import com.example.matt2929.strokeappdec2017.R;
 import com.example.matt2929.strokeappdec2017.SaveAndLoadData.SaveWorkoutJSON;
 import com.example.matt2929.strokeappdec2017.SaveAndLoadData.WorkoutJSON;
 import com.example.matt2929.strokeappdec2017.Utilities.SFXPlayer;
+import com.example.matt2929.strokeappdec2017.Utilities.Text2Speech;
+import com.example.matt2929.strokeappdec2017.Values.WorkoutData;
 import com.example.matt2929.strokeappdec2017.WorkoutsView.GradeView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class PostWorkoutReportActivity extends AppCompatActivity {
+	private final int CHECK_CODE = 0x1;
 	ArrayList<WorkoutJSON> workoutJSONS;
 	SaveWorkoutJSON saveWorkoutJSON;
 	ImageButton imageButton;
 	SFXPlayer sfxPlayer;
+	boolean betterReps = false;
+	boolean betterSmoothness = false;
+	boolean betterTime = false;
+	WorkoutJSON thisWorkout;
+	private Text2Speech _Text2Speech;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -59,17 +70,16 @@ public class PostWorkoutReportActivity extends AppCompatActivity {
 			}
 		};
 
-
 		for (WorkoutJSON workoutJSON : workoutJSONS) {
 			if (workoutJSON.getWorkoutName().equals(getIntent().getStringExtra("Workout"))) {
 				workoutJSONSFiltered.add(workoutJSON);
 			}
 		}
 		Collections.sort(workoutJSONSFiltered, workoutJSONComparator);
-		WorkoutJSON thisWorkout = workoutJSONSFiltered.get(0);
-		boolean betterReps = false;
-		boolean betterSmoothness = false;
-		boolean betterTime = false;
+		thisWorkout = workoutJSONSFiltered.get(0);
+		betterReps = false;
+		betterSmoothness = false;
+		betterTime = false;
 		if (workoutJSONSFiltered.size() >= 2) {
 			WorkoutJSON previousWorkout = workoutJSONSFiltered.get(1);
 			betterReps = (previousWorkout.getReps() <= thisWorkout.getReps());
@@ -84,9 +94,10 @@ public class PostWorkoutReportActivity extends AppCompatActivity {
 			qualityView.SetupView(bitmap2, "Repetition Smoothness (Average)", -1, thisWorkout.getAccuracy(), false);
 			timeView.SetupView(bitmap3, "Repetition Time (Average)", -1, thisWorkout.getDuration(), false);
 		}
+		checkTTS();
 
 		if (betterReps || betterSmoothness || betterTime) {
-			sfxPlayer.playSFX();
+			//		sfxPlayer.playSFX();
 		}
 
 		button.setOnClickListener(new View.OnClickListener() {
@@ -99,4 +110,45 @@ public class PostWorkoutReportActivity extends AppCompatActivity {
 		});
 
 	}
+
+	private void checkTTS() {
+		Intent check = new Intent();
+		check.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(check, CHECK_CODE);
+	}
+
+	//use this to know when tts is done speaking
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CHECK_CODE) {
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+				_Text2Speech = new Text2Speech(this);
+
+				_Text2Speech.addInitListener(new SpeechInitListener() {
+					@Override
+					public void onInit() {
+						_Text2Speech.silence(1000);
+						_Text2Speech.speak("Number of Reps " + floatToHundreth(thisWorkout.getReps()), WorkoutData.TTS_WORKOUT_DESCRIPTION);
+						_Text2Speech.silence(1000);
+						_Text2Speech.speak("Smoothness " + floatToHundreth(thisWorkout.getAccuracy()), WorkoutData.TTS_WORKOUT_DESCRIPTION);
+						_Text2Speech.silence(1000);
+						_Text2Speech.silence(1000);
+						_Text2Speech.speak("Duration " + floatToHundreth(thisWorkout.getDuration()), WorkoutData.TTS_WORKOUT_DESCRIPTION);
+					}
+				});
+
+			} else {
+				Intent install = new Intent();
+				install.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(install);
+			}
+
+		}
+	}
+
+	private String floatToHundreth(float value) {
+		return new DecimalFormat("#.##").format(value);
+	}
+
 }
+
