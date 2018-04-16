d7a24264fb1335b318fc51dde8e11e777285a830
matt2929 <mcstaffo@buffalo.edu>
2018-04-16 16:13:23 -0400

    full screen

--- app/src/main/java/com/example/matt2929/strokeappdec2017/Activity/TouchWorkoutRunner.java
+++ app/src/main/java/com/example/matt2929/strokeappdec2017/Activity/TouchWorkoutRunner.java
@@ -292,7 +292,7 @@ public class TouchWorkoutRunner extends AppCompatActivity {
 // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
 // a general rule, you should design your app to hide the status bar whenever you
 // hide the navigation bar.
-	
+
 
 	}
 
